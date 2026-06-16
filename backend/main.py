1daf35ad826f39691559e5d3336b43ef523f8569
Supriya uppalapati <uppalapatisupriya9@gmail.com>
2026-06-16 17:16:12 +0530

    Update CORS for Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -35,7 +35,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=[
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "https://resume-analyzer-fmnyrvwsg-supriyas-projects-acce2bc7.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
